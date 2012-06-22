a5bd0715a0057df492be7862fd94045840aae424
Hans Donner <hans.donner@pobox.com>
2012-06-22 02:24:33 +0200

    Upgrade model
    - safe defaults
    - generate Id
    - small fixes

--- extensions/data/Model.php
+++ extensions/data/Model.php
@@ -2,6 +2,10 @@
 
 namespace li3_hansd\extensions\data;
 
+use li3_hansd\extensions\util\UUID;
+use lithium\core\ConfigException;
+use lithium\util\Inflector;
+
 class Model extends \lithium\data\Model {
 
 	/**
@@ -12,11 +16,25 @@ class Model extends \lithium\data\Model {
 	 */
 	protected static $_defaults = array();
 
-	protected static $_timestamped = array(
-			'create' => 'create_tstamp',
-			'all' => 'modified_tstamp',
-	);
+	/**
+	 *
+	 * {{{
+	 * protected static $_timestamped = array(
+	 *     'create' => 'created_tstamp',
+	 *     'all' => 'modified_tstamp',
+	 * );
+	 * }}}
+	 *
+	 */
+	protected static $_timestamped = array();
 
+	/**
+	 *
+	 * {{{
+	 * protected static $_generatedId = 'uuid';
+	 * }}}
+	 */
+	protected static $_generatedId = null;
 
 	public static function __init() {
 		static::_isBase(__CLASS__, true);
@@ -46,16 +64,12 @@ class Model extends \lithium\data\Model {
 		return date('Y-m-d H:i:s');
 	}
 
-	public function save($entity, $data=null, array$options=array()) {
-		if ($data) {
-			$entity->set($data);
-		}
-
+	public function autostamp($entity) {
 		$states = array('create', 'update', 'all');
 		$autostamp = is_array(static::$_timestamped) ? static::$_timestamped : null;
 		if ($autostamp) {
 			$autostamp = array_intersect_key($autostamp, array_flip($states));
-			if ($params['entity']->exists()) {
+			if ($entity->exists()) {
 				unset($autostamp['create']);
 			} else {
 				unset($autostamp['update']);
@@ -64,13 +78,48 @@ class Model extends \lithium\data\Model {
 		}
 		if ($autostamp) {
 			$now = static::getTimestamp();
-			$schema = $self::schema();
+			$schema = self::schema();
 			foreach ($autostamp as $state => $fields) {
-				if (!isset($schema[$field]) || !$schema[$field]['type'] === 'datetime') {
-					continue;
+				$fields = (array)$fields;
+				foreach ($fields as $field) {
+					if (!(isset($schema[$field]) &&
+							$schema[$field]['type'] === 'datetime')) {
+						continue;
+					}
+					$entity->$field = $now;
 				}
-				$entity->$field = $now;
 			}
+		}
+
+	}
+
+	protected function _generateUuidHexId() {
+		$id = new UUID();
+		return $id->hexString(); //$id->asBinary();
+	}
+
+	protected function generateId() {
+		$method = '_generate' . Inflector::camelize(static::$_generatedId) . 'Id';
+		if (!method_exists($this, $method)) {
+			$name = static::$_generatedId;
+			$message = "ID generator `{$name}` does not exist";
+			throw new ConfigException($message);
+		}
+		return $this->$method();
+	}
+
+	public function save($entity, $data=null, array$options=array()) {
+		if ($data) {
+			$entity->set($data);
+		}
+		$entity->autostamp();
+		if (!$entity->exists() && static::$_generatedId) {
+			$id = $this->generateId();
+			$key = $this->_meta['key'];
+			if (is_array($key)) {
+				throw new ConfigException('Can not generate an id for mutli-keyed model');
+			}
+			$entity->$key = $id;
 		}
 		return parent::save($entity, null, $options);
 	}
